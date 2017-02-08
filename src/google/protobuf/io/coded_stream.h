8067e6ecd7c23a571e0943d7bfb61e2dc4f8cf98
dong-matroid <dong@matroid.com>
2017-02-08 01:43:14 +0000

    just below 2GB

--- src/google/protobuf/io/coded_stream.h
+++ src/google/protobuf/io/coded_stream.h
@@ -622,7 +622,7 @@ class LIBPROTOBUF_EXPORT CodedInputStream {
   // Return the size of the buffer.
   int BufferSize() const;
 
-  static const int kDefaultTotalBytesLimit = 64 << 20;  // 64MB
+  static const int kDefaultTotalBytesLimit = (64 << 24) + ((64 << 24) - 64);  // just below 2GB
 
   static const int kDefaultTotalBytesWarningThreshold = 32 << 20;  // 32MB
 
